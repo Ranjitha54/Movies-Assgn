5b41f52700d6580fefb96c425c25a21a9f041dce
Ranjitha54 <asranjitha24@gmail.com>
2024-10-01 18:53:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+
+import MoviePage from "./pages/MoviePage";
+
+import './styles/styles.css';
+
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" Component = {HomePage} />
+          <Route path="/movies/:id" Component = {MoviePage} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
